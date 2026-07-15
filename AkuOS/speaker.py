2deee2bfad923ce92e4c6fb82b92d50584882bb9
thomas:) <150758672+mywifisukksbro@users.noreply.github.com>
2026-07-15 13:29:37 -0400

    Add files via upload

---
+++ AkuOS/speaker.py
@@ -0,0 +1,12 @@
+import pyttsx3
+from config import VOICE_RATE
+
+engine = pyttsx3.init()
+
+engine.setProperty("rate", VOICE_RATE)
+
+def speak(text):
+    print(f"Aku: {text}")
+
+    engine.say(text)
+    engine.runAndWait()
